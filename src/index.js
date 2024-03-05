8a332e2e1e842375ea7b1d8374059888778614e4
554282003 <vivek5542810@gmail.com>
2024-03-05 20:03:25 +0530

    file add in utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+const { app } = require('./app');
 const {connectDB} = require('./db/index')
 
 
 connectDB()
 .then(()=>{
-
+    app.listen(process.env.PORT || 8000 , ()=> {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
 })
 .catch((err)=>{
     console.error("MongoDB connection failed!!",err);
