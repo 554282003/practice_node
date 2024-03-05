78200607baac30315a272767cb0461d071703b5f
554282003 <vivek5542810@gmail.com>
2024-03-05 19:31:33 +0530

    change in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const {connectDB} = require('./db/index')
 
 
-<<<<<<< HEAD
 connectDB()
 .then(()=>{
 
@@ -10,6 +9,3 @@ connectDB()
 .catch((err)=>{
     console.error("MongoDB connection failed!!",err);
 });
-=======
-connectDB();
->>>>>>> fe0c9f17ce954d2687a9e9213c60f3d22e8cebc6
